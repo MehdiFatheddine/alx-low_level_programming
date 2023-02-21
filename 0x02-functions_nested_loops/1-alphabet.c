f2037b7a85172daf9b7347a2fc465c5a7de4b593
MehdiFathedddine <fatheddine50@gmail.com>
2023-02-21 03:22:19 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
-#include "main.h"
-/**
- * writed by Mehdi Fatheddine
- * print_alphabet - prints all alphabets in lowercase.
- * Return: On success 1.
- */
-void print_alphabet(void)
-{
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
+#include "1.main.h"
+
+
+	/**
+	 * Writed By Mehdi Fatheddine
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
