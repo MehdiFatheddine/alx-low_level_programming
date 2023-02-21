bba100ed1f5155f40074725e215ad1b3d26b329e
MehdiFathedddine <fatheddine50@gmail.com>
2023-02-21 05:00:58 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return:0 or 1
+ * Writed by Mehdi FATHEDDINE
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
+
